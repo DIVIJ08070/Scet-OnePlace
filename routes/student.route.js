2780bf7d95cf140f1eb49f23303e0cb334549134
Mann Patel <mann.patel.1207@gmail.com>
2025-09-30 11:14:47 +0530

    Apply route

--- routes/student.route.js
+++ routes/student.route.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const studentController = require('../controllers/student.contoller');
 const fileUploadMiddleware = require('../middlewares/fileUpload.middleware');
+const authenticateUser = require('../middlewares/auth.middleware');
 
 // router.post('/', fileUploadMiddleware, studentController.addNewStudent);
 router.post('/', studentController.addNewStudent);
 router.get('/', studentController.retriveAllStudents);
 router.get('/:studentId',studentController.retriveStudentById);
 router.patch('/:studentId',studentController.updateStudent);
-router.post('/apply/:offerId',studentController.applyForOffer);
+router.post('/apply/:offerId', authenticateUser, studentController.applyForOffer);
 
 module.exports = router;    
